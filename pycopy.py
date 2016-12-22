348374281100599cced60af8626d13e587ec3c2f
Andrewerr <Andrewerr@live.ru>
2016-12-22 22:18:47 +0300

    Added comments
    Added comments and checking if os is correct.

--- pycopy.py
+++ pycopy.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-   
-import Foundation
-import objc
-import AppKit
-import sys
-import os
-import termios
-import struct
-import fcntl
 
-
-def set_winsize(row,col):
+#####################################
+#(C)Copyrighting Andrey Zaitsew 2016#
+#####################################
+try:
+    import Foundation
+    import objc
+    import AppKit
+    import sys
+    import os
+    import termios
+    import struct
+    import fcntl
+except ImportError:
+    print "This script could be runned only on Mac OS X 10.7 and later."
+    
+def set_winsize(row,col):#Sets terminal size
     sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=row, cols=col))
     
-def getTerminalSize():
+def getTerminalSize():#get terminal size
     import os
     env = os.environ
     def ioctl_GWINSZ(fd):
@@ -35,17 +40,11 @@ def getTerminalSize():
             pass
     if not cr:
         cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
-
-        ### Use get(key[, default]) instead of a try/catch
-        #try:
-        #    cr = (env['LINES'], env['COLUMNS'])
-        #except:
-        #    cr = (25, 80)
     return int(cr[1]), int(cr[0])   
 
 NSUserNotification = objc.lookUpClass('NSUserNotification')
 NSUserNotificationCenter = objc.lookUpClass('NSUserNotificationCenter')
-def notify(title, subtitle, info_text, delay=0, sound=False, userInfo={}):
+def notify(title, subtitle, info_text, delay=0, sound=False, userInfo={}):#notifier functiob
     notification = NSUserNotification.alloc().init()
     notification.setTitle_(title)
     notification.setSubtitle_(subtitle)
@@ -56,20 +55,20 @@ def notify(title, subtitle, info_text, delay=0, sound=False, userInfo={}):
     notification.setDeliveryDate_(Foundation.NSDate.dateWithTimeInterval_sinceDate_(delay, Foundation.NSDate.date()))
     NSUserNotificationCenter.defaultUserNotificationCenter().scheduleNotification_(notification)
     
-global SOURCE_FILENAME
-global TARGET_FILENAME
+global SOURCE_FILENAME#name of source filename
+global TARGET_FILENAME#name of target filename
 
-def main(argv):
+def main(argv):#main function
   global SOURCE_FILENAME
   global TARGET_FILENAME
   row, col = getTerminalSize()
   #print str(rows)+","+str(cols)
   if col<110:
      set_winsize(row,110)
-  if(len(argv)>1):
+  if(len(argv)>1):# if enough arguments we're going to use them
  	 SOURCE_FILENAME=argv[1]
  	 TARGET_FILENAME=argv[2]
-  else:
+  else:#not enough arguments so asking user usin manual input
      SOURCE_FILENAME=raw_input("SOURCE:")
      TARGET_FILENAME=raw_input("DEST:")
   source_size = os.stat(SOURCE_FILENAME).st_size
@@ -81,18 +80,18 @@ def main(argv):
   while True:
       chunk = source.read(32768)
       if not chunk:
-          break
+          break#EOF
       target.write(chunk)
       copied += len(chunk)
-      p=(copied * 100 / source_size)
-      print '\r['+'▋'*p+' '*(100-p)+']%02d%%' % p+spinner[i],
+      p=(copied * 100 / source_size)#counting progress
+      print '\r['+'▋'*p+' '*(100-p)+']%02d%%' % p+spinner[i],#printing progress
       if i>=3:
          i=0
       else:
          i=i+1
-  source.close()
+  source.close()#Closing files
   target.close()
-if __name__=="__main__":
+if __name__=="__main__":#if running as script
   try:
       global SOURCE_FILENAME
       global TARGET_FILENAME
@@ -103,3 +102,5 @@ if __name__=="__main__":
       print "Operation terminated!"
   except OSError,IOError:
       print "Could not open file!Check if file exists and you have right permissions."
+  except Exception as e:
+      print str(e)
