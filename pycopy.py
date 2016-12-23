0a6051f730e2844cd7a07f6cd723f0c9266ccfe4
Andrewerr <Andrewerr@live.ru>
2016-12-23 12:14:07 +0300

    Added -u key
    Now it could also download files from internet.

--- pycopy.py
+++ pycopy.py
@@ -13,12 +13,43 @@ try:
     import termios
     import struct
     import fcntl
+    import time
 except ImportError:
     print "This script could be runned only on Mac OS X 10.7 and later."
-    
+
 def set_winsize(row,col):#Sets terminal size
     sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=row, cols=col))
-    
+def download(link,file_name):
+    try:
+          import requests
+    except ImportError:
+          ans=raw_input("This feature requires requests library.Would you like install it now?[Y/N]:").lower()
+          if ans=="y":
+            from subprocess import call
+            sys.exit(call(["sudo","easy_install","requests"]))
+    start=time.clock()#we need it to measure speed
+    with open(file_name, "wb") as f:
+          print "Downloading to %s" % file_name
+          response = requests.get(link, stream=True)
+          total_length = response.headers.get('content-length')
+
+          if total_length is None: # no content length header
+              f.write(response.content)
+          else:
+              dl = 0 #downloaded length
+              total_length = int(total_length)
+              i=0
+              spinner=['|','/','-','\\']
+              for data in response.iter_content(chunk_size=4096):
+                  dl += len(data)
+                  f.write(data)# writing to file
+                  done = int(100 * dl / total_length)#average of percents done
+                  sys.stdout.write("\r["+'▋'*done+' '*(100-done)+"]%02d%%"%done+"("+str(dl//1024//1024)+"/"+str(total_length//1024//1024)+"Mb),"+str(dl//(time.clock() - start)//1024)+"Kb/s"+spinner[i] )    
+                  sys.stdout.flush()
+                  if i>=3:
+                    i=0
+                  else:
+                    i=i+1
 def getTerminalSize():#get terminal size
     import os
     env = os.environ
@@ -44,7 +75,7 @@ def getTerminalSize():#get terminal size
 
 NSUserNotification = objc.lookUpClass('NSUserNotification')
 NSUserNotificationCenter = objc.lookUpClass('NSUserNotificationCenter')
-def notify(title, subtitle, info_text, delay=0, sound=False, userInfo={}):#notifier functiob
+def notify(title, subtitle, info_text, delay=0, sound=False, userInfo={}):#notifier function
     notification = NSUserNotification.alloc().init()
     notification.setTitle_(title)
     notification.setSubtitle_(subtitle)
@@ -63,8 +94,12 @@ def main(argv):#main function
   global TARGET_FILENAME
   row, col = getTerminalSize()
   #print str(rows)+","+str(cols)
-  if col<110:
-     set_winsize(row,110)
+  if col<140:
+     set_winsize(row,140)
+  if "-u" in argv:
+     download(argv[2],argv[3])
+     print "Download complete!"
+     sys.exti(0)
   if(len(argv)>1):# if enough arguments we're going to use them
  	 SOURCE_FILENAME=argv[1]
  	 TARGET_FILENAME=argv[2]
