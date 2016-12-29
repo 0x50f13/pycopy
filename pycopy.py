8d5eecf42dd159867f1a3acbecb8ef5e75a3c030
andre <andre@linux-2hd2.site>
2016-12-29 12:33:31 +0300

    commit

--- pycopy.py
+++ pycopy.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-#####################################
-#(C)Copyrighting Andrey Zaitsew 2016#
-#####################################
+##############################################
+#(C)Copyrighting Andrey Zaitsew 2016         #
+#Licensed under GNU/GPL 3.0                  #
+#https://www.gnu.org/licenses/gpl-3.0.html   #
+##############################################
+
+###############################
+#This script is for python 2.7#
+#Branch for linux computers   #
+###############################
 try:
-    import Foundation
-    import objc
-    import AppKit
     import sys
     import os
-    import termios
-    import struct
-    import fcntl
     import time
-except ImportError:
-    print "This script could be runned only on Mac OS X 10.7 and later."
+except ImportError as e:
+    print "Could not run script:library not found("+str(e)+")"
 
-def set_winsize(row,col):#Sets terminal size
-    sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=row, cols=col))
+
 def download(link,file_name):
     try:
           import requests
@@ -50,52 +50,20 @@ def download(link,file_name):
                     i=0
                   else:
                     i=i+1
-def getTerminalSize():#get terminal size
-    import os
-    env = os.environ
-    def ioctl_GWINSZ(fd):
-        try:
-            import fcntl, termios, struct, os
-            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
-        '1234'))
-        except:
-            return
-        return cr
-    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
-    if not cr:
-        try:
-            fd = os.open(os.ctermid(), os.O_RDONLY)
-            cr = ioctl_GWINSZ(fd)
-            os.close(fd)
-        except:
-            pass
-    if not cr:
-        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
-    return int(cr[1]), int(cr[0])   
-
-NSUserNotification = objc.lookUpClass('NSUserNotification')
-NSUserNotificationCenter = objc.lookUpClass('NSUserNotificationCenter')
-def notify(title, subtitle, info_text, delay=0, sound=False, userInfo={}):#notifier function
-    notification = NSUserNotification.alloc().init()
-    notification.setTitle_(title)
-    notification.setSubtitle_(subtitle)
-    notification.setInformativeText_(info_text)
-
-    if sound:
-        notification.setSoundName_("NSUserNotificationDefaultSoundName")
-    notification.setDeliveryDate_(Foundation.NSDate.dateWithTimeInterval_sinceDate_(delay, Foundation.NSDate.date()))
-    NSUserNotificationCenter.defaultUserNotificationCenter().scheduleNotification_(notification)
-    
+                    
 global SOURCE_FILENAME#name of source filename
 global TARGET_FILENAME#name of target filename
+#commit me
 
+def ver():
+  print "Version:1.0.0-prerelease"
+  sys.exit(0)
+ver()
 def main(argv):#main function
   global SOURCE_FILENAME
   global TARGET_FILENAME
-  row, col = getTerminalSize()
-  #print str(rows)+","+str(cols)
-  if col<140:
-     set_winsize(row,140)
+  if "-v" in argv:
+     ver()
   if "-u" in argv:
      download(argv[2],argv[3])
      print "\nDownload complete!"
@@ -132,7 +100,6 @@ if __name__=="__main__":#if running as script
       global TARGET_FILENAME
       main(sys.argv)
       print "\n\aDone!"
-      notify("Copy"," ","Succesful copied "+os.path.splitext(SOURCE_FILENAME)[0]+" from "+os.path.dirname(os.path.abspath(SOURCE_FILENAME))+" to "+os.path.dirname(os.path.abspath(TARGET_FILENAME))+" as "+os.path.splitext(TARGET_FILENAME)[0])
   except KeyboardInterrupt:
       print "Operation terminated!"
   except OSError,IOError:
